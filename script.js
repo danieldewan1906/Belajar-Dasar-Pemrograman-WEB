97522e045ce5fc53ff0638ffb29b2e2873bac89d
danieldewanto1906 <danieldewanto1906@gmail.com>
2022-07-28 18:56:31 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+function foto2cartenz() {
+    this.setAttribute("src", "assets/image/jalur-cartenz.jpg");
+}
+
+function fotocartenz() {
+    this.setAttribute("src", "assets/image/cartenz.jpg");
+}
+
+function foto2kerinci() {
+    this.setAttribute("src", "assets/image/jalur-kerinci.jpg");
+}
+
+function fotokerinci() {
+    this.setAttribute("src", "assets/image/kerinci.jpg");
+}
+
+function foto2semeru() {
+    this.setAttribute("src", "assets/image/jalur-semeru.png");
+}
+
+function fotosemeru() {
+    this.setAttribute("src", "assets/image/semeru.jpg");
+}
+
+function foto2rinjani() {
+    this.setAttribute("src", "assets/image/jalur-rinjani.jpg");
+}
+
+function fotorinjani() {
+    this.setAttribute("src", "assets/image/rinjani.jpg");
+}
+
+function foto2bukit() {
+    this.setAttribute("src", "assets/image/jalur-bukitraya.jpg");
+}
+
+function fotobukit() {
+    this.setAttribute("src", "assets/image/bukitraya.jpg");
+}
+
+function foto2latimojong() {
+    this.setAttribute("src", "assets/image/jalur-latimojong.jpeg");
+}
+
+function fotolatimojong() {
+    this.setAttribute("src", "assets/image/latimojong.png");
+}
+
+function foto2binaiya() {
+    this.setAttribute("src", "assets/image/jalur-binaiya.jpg");
+}
+
+function fotobinaiya() {
+    this.setAttribute("src", "assets/image/binaiya.jpg");
+}
